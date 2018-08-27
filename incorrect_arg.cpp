b6d622876def2a2ccc2638ab01edd4435c208d29
Andrew Stepanov <stepanoff.work@gmail.com>
2018-08-27 15:18:20 +0700

    Update incorrect_arg.cpp

--- incorrect_arg.cpp
+++ incorrect_arg.cpp
@@ -1,9 +1,11 @@
 #include "header.h"
 
 //Проверяю введенные аргументы на соответствие требованиям
-int incorrect_arg(int argc,char *argv[]){										
-	if(argc==2 && strcmp(argv[1],"-h")==0){													//1)Если помимо имени программы передан только один аргумент, 
-		printf("\t[HELP:]\n");																//этим аргументом может быть только опция для вызова справки [-h]
+int incorrect_arg(int argc,char *argv[]){
+	//1)Если помимо имени программы передан только один аргумент,
+	//этим аргументом может быть только опция для вызова справки [-h]
+	if(argc==2 && strcmp(argv[1],"-h")==0){													 
+		printf("\t[HELP:]\n");																
 		printf("First two arguments must be Input and Output file respectively.\n");
 		printf("You can use only one option for content and one for sorting mode at a time.\n");
 		printf("Options:\n");
@@ -14,24 +16,29 @@ int incorrect_arg(int argc,char *argv[]){
 		printf("To view help, use [-h]\n");
 		return	1;
 	}
-	else if(argc!=5){												//2)Если аргументов недостаточное количество
+	//2)Если аргументов недостаточное количество
+	else if(argc!=5){												
 		printf("Error - incorrect number of arguments!\n");
 		printf("To view help, use [-h]\n");
 		return 1;
 	}
-	else if(argc==5){												//3)Если количесвто аргументов правильное,но  
-		int content=0, mode=0;										//нарушена логика работы программы с опциями:
+	//3)Если количесвто аргументов правильное,но  
+	//нарушена логика работы программы с опциями:
+	else if(argc==5){												
+		int content=0, mode=0;										
 		for(int i=0; i<argc; i++){
 			if(strcmp(argv[i], "-s")==0 || strcmp(argv[i], "-i")==0)	
-				content++;												//Подсчет количества используемых опций для содержимого
+				content++; //Подсчет количества используемых опций для содержимого												
 			if(strcmp(argv[i], "-a")==0 || strcmp(argv[i], "-d")==0)	
-				mode++;													//Подсчет количества используемых опций для режима сортировки
+				mode++;	   //Подсчет количества используемых опций для режима сортировки												
 		}
-		if(content>1 && mode>1){									//3.1)Если одновременно выбраны все 4 опции					
+		//3.1)Если одновременно выбраны все 4 опции
+		if(content>1 && mode>1){														
 			printf("Using all options at once is forbidden. To view help, use [-h]\n");
 			return	1;
 		} 
-		else if(content>1 || mode>1){								//3.2)Если одновременно выбраны взаимоисключающие варианты
+		//3.2)Если одновременно выбраны взаимоисключающие варианты
+		else if(content>1 || mode>1){								
 			printf("Please,select only one of the two options.\n");
 			return	1;
 		} 

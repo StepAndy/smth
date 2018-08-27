0398c7b194389a917af264ea0ac3609cdecc7f23
Andrew Stepanov <stepanoff.work@gmail.com>
2018-08-27 15:14:48 +0700

    Update get_options.cpp

--- get_options.cpp
+++ get_options.cpp
@@ -1,8 +1,8 @@
 #include "header.h"
 
 //Получение и работа с опциями
-int get_options(int argc, char *argv[],bool &contents_string,bool &ascending_mode){		//Последние два параметра - ссылочные (чтобы изменять оригинальные данные, а не их копии)	
-	const char *options = "siad";														//"siad" - Допустимые опции,написанные без пробелов  										
+int get_options(int argc, char *argv[],bool &contents_string,bool &ascending_mode){		
+	const char *options = "siad";																					
 	int opt_argument = 0;
 	while((opt_argument = getopt(argc,argv,options))!=-1){
 		switch(opt_argument){

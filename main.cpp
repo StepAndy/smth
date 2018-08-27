abef7c5721bbaad6b595ad3a2decc874f8164a41
Andrew Stepanov <stepanoff.work@gmail.com>
2018-08-27 15:08:20 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,8 +20,10 @@ int in_to_buf(int &count, bool contents_string,ifstream &in,string *buf_str,int 
 	while(!in.eof()){
 		int j=0;
 		if(contents_string){
-			getline(in,buf_str[i]); 	//Пока не закончился файл, заполняю буфер его содержимым
-			while(buf_str[i][j]){ 		//До конца строки проверяю на отсутствие пробелов, иначе сообщаю об ошибке в содержимом
+			//Пока не закончился файл, заполняю буфер его содержимым
+			getline(in,buf_str[i]); 	
+			//До конца строки проверяю на отсутствие пробелов, иначе сообщаю об ошибке в содержимом
+			while(buf_str[i][j]){ 		
 				if(buf_str[i][j]==' '){											
 					printf("Incorrect file content! File must not have spaces.\n");							
 					return 0;
@@ -32,7 +34,8 @@ int in_to_buf(int &count, bool contents_string,ifstream &in,string *buf_str,int 
 		} 
 		else{
 			in >> buf_int[i++];																												
-			if(!isdigit(buf_int[i])){	//Если вместо чисел другое содержимое, объявить об ошибке
+			//Если вместо чисел другое содержимое, объявить об ошибке
+			if(!isdigit(buf_int[i])){	
 				printf("Incorrect file content! The content must consist of numbers only.\n");
 				return 0;
 			}
@@ -60,30 +63,36 @@ int main(int argc, char *argv[])
 	int i=0, count=0;	
 	bool contents_string,ascending_mode; 
 	
+	//Выход, если аргументы переданы некорректно
 	if(incorrect_arg(argc,argv)) 
-		return 1;															//Выход, если аргументы переданы некорректно
-	else get_options(argc,argv,contents_string,ascending_mode);				//Если все верно, то работать в соответствии с полученными аргументами 	
+		return 1;															
+	//Если все верно, то работать в соответствии с полученными аргументами 	
+	else get_options(argc,argv,contents_string,ascending_mode);				
 	
-	
-	if(input_file_error(in)) return 1;										//Выход, если нет указанного входного файла									
-	if(!in_to_buf(count,contents_string,in,buf_str,buf_int)) return 1;		//Копировать в буфер из входного файла. Выйти, если возникло исключение.
+	//Выход, если нет указанного входного файла
+	if(input_file_error(in)) return 1;	
+	//Копировать в буфер из входного файла. Выйти, если возникло исключение
+	if(!in_to_buf(count,contents_string,in,buf_str,buf_int)) return 1;		
 	in.close();
 	
 	if(contents_string)	
-		isort_str(buf_str,count);											//Сортирую буфер в зависимости от содержимого соответствующей функцией
+		//Сортирую буфер в зависимости от содержимого соответствующей функцией
+		isort_str(buf_str,count);											
 	else 
 		isort_int(buf_int,count);
 
-	if(!ascending_mode){													//Если сортировка не по возрастанию, 										
-		int j=0;															//а по убыванию - массив перезаписывается в обратном порядке.
+	//Если сортировка по убыванию -  массив перезаписывается в обратном порядке 	
+	if(!ascending_mode){																						
+		int j=0;															
 		if(contents_string) 
 			str_descending(buf_str,count);
 		else 
 			int_descending(buf_int,count);		
 	}
 	
-	ofstream out(argv[argc-1]);												
-	buf_to_file(count,contents_string,buf_str,buf_int,out);					//Передаю из буфера содержимое в выходной файл
+	ofstream out(argv[argc-1]);	
+	//Передаю из буфера содержимое в выходной файл
+	buf_to_file(count,contents_string,buf_str,buf_int,out);					
 	out.close();
 	return 0;
 }
